0159953f1f33b4aa65afa4a34886eabd38d3c503
sachaa38 <santoniotto@gmail.com>
2026-01-13 21:50:57 +0100

    Fix: disabling trailingSlash to match existing links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-/** @type {import('next').Next.Config} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: false, // ON LE REMET : c'est lui qui crée les dossiers propres
-  images: { unoptimized: true },
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",             // export statique
+  // basePath: "/PhotoK",          // nom du repo GitHub
+  // assetPrefix: "/PhotoK/",      // pour que CSS/JS soient trouvés
+  images: { unoptimized: true }, // pour ne pas utiliser le loader Next.js
 };
+
 export default nextConfig;
